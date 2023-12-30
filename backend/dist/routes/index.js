57d0458d8a13265fb108630ab15427eb0e8800c4
Hung-En Liao <Hung-En Liao>
2023-12-30 13:20:06 -0400

    remove js dictionary from git

--- backend/dist/routes/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PinRouter = void 0;
-var pin_1 = require("./pin");
-Object.defineProperty(exports, "PinRouter", { enumerable: true, get: function () { return pin_1.router; } });
--- backend/dist/routes/pin.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const pin_1 = require("../controllers/pin");
-const pin_2 = require("../middleware/validation/pin");
-exports.router = (0, express_1.Router)();
-exports.router.get("/", pin_1.getAllPins);
-exports.router.post("/", (0, pin_2.validationPinRequest)(pin_2.Schemas.data), pin_1.createPin);
